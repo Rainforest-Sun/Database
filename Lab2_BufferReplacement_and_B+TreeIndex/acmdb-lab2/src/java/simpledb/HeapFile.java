c64f744f63cab0c287a0d71c42d2f4e72e9f18e1
Rainforest-Sun <1366819881@qq.com>
2024-04-14 16:26:33 +0800

    init lab2

---
+++ Lab2_BufferReplacement_and_B+TreeIndex/acmdb-lab2/src/java/simpledb/HeapFile.java
@@ -0,0 +1,192 @@
+package simpledb;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * HeapFile is an implementation of a DbFile that stores a collection of tuples
+ * in no particular order. Tuples are stored on pages, each of which is a fixed
+ * size, and the file is simply a collection of those pages. HeapFile works
+ * closely with HeapPage. The format of HeapPages is described in the HeapPage
+ * constructor.
+ * 
+ * @see simpledb.HeapPage#HeapPage
+ * @author Sam Madden
+ */
+public class HeapFile implements DbFile {
+
+    private File file;
+    private TupleDesc td;
+    // private ConcurrentHashMap<PageId, Page> PageIdToPage;
+
+    /**
+     * Constructs a heap file backed by the specified file.
+     * 
+     * @param f
+     *            the file that stores the on-disk backing store for this heap
+     *            file.
+     */
+    public HeapFile(File f, TupleDesc td) {
+        // some code goes here
+        this.file = f;
+        this.td = td;
+        // this.PageIdToPage = new ConcurrentHashMap<>();
+    }
+
+    /**
+     * Returns the File backing this HeapFile on disk.
+     * 
+     * @return the File backing this HeapFile on disk.
+     */
+    public File getFile() {
+        // some code goes here
+        return this.file;
+    }
+
+    /**
+     * Returns an ID uniquely identifying this HeapFile. Implementation note:
+     * you will need to generate this tableid somewhere ensure that each
+     * HeapFile has a "unique id," and that you always return the same value for
+     * a particular HeapFile. We suggest hashing the absolute file name of the
+     * file underlying the heapfile, i.e. f.getAbsoluteFile().hashCode().
+     * 
+     * @return an ID uniquely identifying this HeapFile.
+     */
+    public int getId() {
+        // some code goes here
+        // throw new UnsupportedOperationException("implement this");
+        return this.file.getAbsoluteFile().hashCode();
+    }
+
+    /**
+     * Returns the TupleDesc of the table stored in this DbFile.
+     * 
+     * @return TupleDesc of this DbFile.
+     */
+    public TupleDesc getTupleDesc() {
+        // some code goes here
+        // throw new UnsupportedOperationException("implement this");
+        return this.td;
+    }
+
+    // see DbFile.java for javadocs
+    public Page readPage(PageId pid) {
+        // some code goes here
+        try {
+            FileInputStream fis = new FileInputStream(this.file);
+            byte[] data = new byte[BufferPool.getPageSize()];
+            fis.skip(pid.pageNumber() * BufferPool.getPageSize());
+            fis.read(data);
+            fis.close();
+
+            Page page = new HeapPage((HeapPageId) pid, data);
+            return page;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // see DbFile.java for javadocs
+    public void writePage(Page page) throws IOException {
+        // some code goes here
+        // not necessary for lab1
+    }
+
+    /**
+     * Returns the number of pages in this HeapFile.
+     */
+    public int numPages() {
+        // some code goes here
+        return (int) Math.ceil(this.file.length() / BufferPool.getPageSize());
+    }
+
+    // see DbFile.java for javadocs
+    public ArrayList<Page> insertTuple(TransactionId tid, Tuple t)
+            throws DbException, IOException, TransactionAbortedException {
+        // some code goes here
+        return null;
+        // not necessary for lab1
+    }
+
+    // see DbFile.java for javadocs
+    public ArrayList<Page> deleteTuple(TransactionId tid, Tuple t) throws DbException,
+            TransactionAbortedException {
+        // some code goes here
+        return null;
+        // not necessary for lab1
+    }
+
+    /**
+     * An auxiliary class that implements the Java Iterator for tuples on a page
+     */
+    public class HeapFileIterator implements DbFileIterator {
+        private Iterator<Tuple> tupleIterator;
+        private TransactionId tid;
+        private PageId curPid;
+        private boolean isOpen;
+
+        public HeapFileIterator(TransactionId tid) {
+            this.tid = tid;
+            this.isOpen = false;
+        }
+
+        @Override
+        public void open() throws DbException, TransactionAbortedException {
+            this.curPid = new HeapPageId(getId(), 0);
+            this.tupleIterator = ((HeapPage) Database.getBufferPool().getPage(tid, curPid, Permissions.READ_ONLY)).iterator();
+            this.isOpen = true;
+        }
+
+        @Override
+        public boolean hasNext() throws DbException, TransactionAbortedException {
+            if (!this.isOpen) {
+                return false;
+            }
+            if (this.tupleIterator.hasNext()) {
+                return true;
+            }
+            if (this.curPid.pageNumber() + 1 >= numPages()) {
+                return false;
+            }
+            int nextPageNo = this.curPid.pageNumber() + 1;
+            PageId nextPid = new HeapPageId(getId(), nextPageNo);
+            Iterator<Tuple> nextTupleIterator = ((HeapPage) Database.getBufferPool().getPage(tid, nextPid, Permissions.READ_ONLY)).iterator();
+            return nextTupleIterator.hasNext();
+        }
+
+        @Override
+        public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            if (this.tupleIterator.hasNext()) return this.tupleIterator.next();
+            this.curPid = new HeapPageId(getId(), this.curPid.pageNumber() + 1);
+            this.tupleIterator = ((HeapPage) Database.getBufferPool().getPage(tid, curPid, Permissions.READ_ONLY)).iterator();
+            return this.tupleIterator.next();
+        }
+
+        @Override
+        public void rewind() throws DbException, TransactionAbortedException {
+            close();
+            open();
+        }
+
+        @Override
+        public void close() {
+            this.isOpen = false;
+        }
+    }
+
+    // see DbFile.java for javadocs
+    public DbFileIterator iterator(TransactionId tid) {
+        // some code goes here
+        return new HeapFileIterator(tid);
+    }
+
+}
+
---
+++ Lab2_BufferReplacement_and_B+TreeIndex/acmdb-lab2/src/java/simpledb/TupleDesc.java
@@ -0,0 +1,268 @@
+package simpledb;
+
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * TupleDesc describes the schema of a tuple.
+ */
+public class TupleDesc implements Serializable {
+
+    /**
+     * A help class to facilitate organizing the information of each field
+     * */
+    public static class TDItem implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * The type of the field
+         * */
+        public final Type fieldType;
+        
+        /**
+         * The name of the field
+         * */
+        public final String fieldName;
+
+        public TDItem(Type t, String n) {
+            this.fieldName = n;
+            this.fieldType = t;
+        }
+
+        public String toString() {
+            return fieldName + "(" + fieldType + ")";
+        }
+    }
+
+    // List of TDItems
+    private final List<TDItem> tdItems;
+
+    /**
+     * @return
+     *        An iterator which iterates over all the field TDItems
+     *        that are included in this TupleDesc
+     * */
+    public Iterator<TDItem> iterator() {
+        // some code goes here
+        return tdItems.iterator();
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Create a new TupleDesc with typeAr.length fields with fields of the
+     * specified types, with associated named fields.
+     * 
+     * @param typeAr
+     *            array specifying the number of and types of fields in this
+     *            TupleDesc. It must contain at least one entry.
+     * @param fieldAr
+     *            array specifying the names of the fields. Note that names may
+     *            be null.
+     */
+    public TupleDesc(Type[] typeAr, String[] fieldAr) {
+        // some code goes here
+        this.tdItems = new ArrayList<TDItem>();
+        String[] _fieldAr;
+        if (fieldAr == null) {
+            _fieldAr = new String[typeAr.length];
+        }
+        else {
+            _fieldAr = fieldAr;
+        }
+        for (int i = 0; i < typeAr.length; i++) {
+            tdItems.add(new TDItem(typeAr[i], _fieldAr[i]));
+        }
+    }
+
+    /**
+     * Constructor. Create a new tuple desc with typeAr.length fields with
+     * fields of the specified types, with anonymous (unnamed) fields.
+     * 
+     * @param typeAr
+     *            array specifying the number of and types of fields in this
+     *            TupleDesc. It must contain at least one entry.
+     */
+    public TupleDesc(Type[] typeAr) {
+        // some code goes here
+        this.tdItems = new ArrayList<TDItem>();
+        for (int i = 0; i < typeAr.length; i++) {
+            tdItems.add(new TDItem(typeAr[i], null));
+        }
+    }
+
+    /**
+     * @return the number of fields in this TupleDesc
+     */
+    public int numFields() {
+        // some code goes here
+        return tdItems.size();
+    }
+
+    /**
+     * Gets the (possibly null) field name of the ith field of this TupleDesc.
+     * 
+     * @param i
+     *            index of the field name to return. It must be a valid index.
+     * @return the name of the ith field
+     * @throws NoSuchElementException
+     *             if i is not a valid field reference.
+     */
+    public String getFieldName(int i) throws NoSuchElementException {
+        // some code goes here
+        try {
+            return tdItems.get(i).fieldName;
+        } catch (IndexOutOfBoundsException e) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    /**
+     * Gets the type of the ith field of this TupleDesc.
+     * 
+     * @param i
+     *            The index of the field to get the type of. It must be a valid
+     *            index.
+     * @return the type of the ith field
+     * @throws NoSuchElementException
+     *             if i is not a valid field reference.
+     */
+    public Type getFieldType(int i) throws NoSuchElementException {
+        // some code goes here
+        try {
+            return tdItems.get(i).fieldType;
+        } catch (IndexOutOfBoundsException e) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    /**
+     * Find the index of the field with a given name.
+     * 
+     * @param name
+     *            name of the field.
+     * @return the index of the field that is first to have the given name.
+     * @throws NoSuchElementException
+     *             if no field with a matching name is found.
+     */
+    public int fieldNameToIndex(String name) throws NoSuchElementException {
+        // some code goes here
+        Iterator<TDItem> itr = tdItems.iterator();
+        while (itr.hasNext()) {
+            TDItem tdItem = itr.next();
+            if (tdItem.fieldName == null) {
+                if (name == null) {
+                    return tdItems.indexOf(tdItem);
+                }
+            }
+            else if (tdItem.fieldName.equals(name)) {
+                return tdItems.indexOf(tdItem);
+            }
+        }
+        throw new NoSuchElementException();
+    }
+
+    /**
+     * @return The size (in bytes) of tuples corresponding to this TupleDesc.
+     *         Note that tuples from a given TupleDesc are of a fixed size.
+     */
+    public int getSize() {
+        // some code goes here
+        int totalSize = 0;
+        Iterator<TDItem> itr = tdItems.iterator();
+        while (itr.hasNext()) {
+            TDItem tdItem = itr.next();
+            totalSize += tdItem.fieldType.getLen();
+        }
+        return totalSize;
+    }
+
+    /**
+     * Merge two TupleDescs into one, with td1.numFields + td2.numFields fields,
+     * with the first td1.numFields coming from td1 and the remaining from td2.
+     * 
+     * @param td1
+     *            The TupleDesc with the first fields of the new TupleDesc
+     * @param td2
+     *            The TupleDesc with the last fields of the TupleDesc
+     * @return the new TupleDesc
+     */
+    public static TupleDesc merge(TupleDesc td1, TupleDesc td2) {
+        // some code goes here
+        Type[] typeAr = new Type[td1.numFields() + td2.numFields()];
+        String[] fieldAr = new String[td1.numFields() + td2.numFields()];
+        for (int i = 0; i < td1.numFields(); i++) {
+            typeAr[i] = td1.getFieldType(i);
+            fieldAr[i] = td1.getFieldName(i);
+        }
+        for (int i = 0; i < td2.numFields(); i++) {
+            typeAr[td1.numFields() + i] = td2.getFieldType(i);
+            fieldAr[td1.numFields() + i] = td2.getFieldName(i);
+        }
+        return new TupleDesc(typeAr, fieldAr);
+    }
+
+    /**
+     * Compares the specified object with this TupleDesc for equality. Two
+     * TupleDescs are considered equal if they are the same size and if the n-th
+     * type in this TupleDesc is equal to the n-th type in td.
+     * 
+     * @param o
+     *            the Object to be compared for equality with this TupleDesc.
+     * @return true if the object is equal to this TupleDesc.
+     */
+    public boolean equals(Object o) {
+        // some code goes here
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (this.getClass() != o.getClass())
+            return false;
+        TupleDesc other = (TupleDesc) o;
+        if (this.numFields() != other.numFields()) {
+            return false;
+        }
+        for (int i = 0; i < this.numFields(); i++) {
+            if (this.getFieldType(i) != other.getFieldType(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int hashCode() {
+        // If you want to use TupleDesc as keys for HashMap, implement this so
+        // that equal objects have equals hashCode() results
+        // throw new UnsupportedOperationException("unimplemented");
+        Iterator<TDItem> itr = tdItems.iterator();
+        int hash = 0;
+        while (itr.hasNext()) {
+            TDItem tdItem = itr.next();
+            hash = hash * 31 + tdItem.fieldType.hashCode();
+        }
+        return hash;
+    }
+
+    /**
+     * Returns a String describing this descriptor. It should be of the form
+     * "fieldType[0](fieldName[0]), ..., fieldType[M](fieldName[M])", although
+     * the exact format does not matter.
+     * 
+     * @return String describing this descriptor.
+     */
+    public String toString() {
+        // some code goes here
+        StringBuffer sb = new StringBuffer();
+        Iterator<TDItem> itr = tdItems.iterator();
+        while (itr.hasNext()) {
+            TDItem tdItem = itr.next();
+            sb.append(tdItem.toString());
+            if (itr.hasNext()) {
+                sb.append(", ");
+            }
+        }
+        return sb.toString();
+    }
+}
