d5a1430cff136e594d7b647fe2505a647404e556
Rainforest-Sun <1366819881@qq.com>
2024-05-09 15:57:22 +0800

    finish lab3 exe1

---
+++ Lab3_SimpleDB_Operators/acmdb-lab3/src/java/simpledb/Tuple.java
@@ -0,0 +1,125 @@
+package simpledb;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Tuple maintains information about the contents of a tuple. Tuples have a
+ * specified schema specified by a TupleDesc object and contain Field objects
+ * with the data for each field.
+ */
+public class Tuple implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private TupleDesc td;
+    private RecordId rid;
+    private ArrayList<Field> fields;
+
+    /**
+     * Create a new tuple with the specified schema (type).
+     *
+     * @param td
+     *            the schema of this tuple. It must be a valid TupleDesc
+     *            instance with at least one field.
+     */
+    public Tuple(TupleDesc td) {
+        // some code goes here
+        this.td = td;
+        this.fields = new ArrayList<Field>(Collections.nCopies(td.numFields(), null));
+    }
+
+    /**
+     * @return The TupleDesc representing the schema of this tuple.
+     */
+    public TupleDesc getTupleDesc() {
+        // some code goes here
+        return this.td;
+    }
+
+    /**
+     * @return The RecordId representing the location of this tuple on disk. May
+     *         be null.
+     */
+    public RecordId getRecordId() {
+        // some code goes here
+        return this.rid;
+    }
+
+    /**
+     * Set the RecordId information for this tuple.
+     *
+     * @param rid
+     *            the new RecordId for this tuple.
+     */
+    public void setRecordId(RecordId rid) {
+        // some code goes here
+        this.rid = rid;
+    }
+
+    /**
+     * Change the value of the ith field of this tuple.
+     *
+     * @param i
+     *            index of the field to change. It must be a valid index.
+     * @param f
+     *            new value for the field.
+     */
+    public void setField(int i, Field f) {
+        // some code goes here
+        this.fields.set(i, f);
+    }
+
+    /**
+     * @return the value of the ith field, or null if it has not been set.
+     *
+     * @param i
+     *            field index to return. Must be a valid index.
+     */
+    public Field getField(int i) {
+        // some code goes here
+        return this.fields.get(i);
+    }
+
+    /**
+     * Returns the contents of this Tuple as a string. Note that to pass the
+     * system tests, the format needs to be as follows:
+     *
+     * column1\tcolumn2\tcolumn3\t...\tcolumnN
+     *
+     * where \t is any whitespace (except a newline)
+     */
+    public String toString() {
+        // some code goes here
+        // throw new UnsupportedOperationException("Implement this");
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < this.fields.size(); i++) {
+            sb.append(this.fields.get(i).toString());
+            if (i != this.fields.size() - 1) {
+                sb.append("\t");
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * @return
+     *        An iterator which iterates over all the fields of this tuple
+     * */
+    public Iterator<Field> fields()
+    {
+        // some code goes here
+        return this.fields.iterator();
+    }
+
+    /**
+     * reset the TupleDesc of thi tuple
+     * */
+    public void resetTupleDesc(TupleDesc td)
+    {
+        // some code goes here
+        this.td = td;
+    }
+}
